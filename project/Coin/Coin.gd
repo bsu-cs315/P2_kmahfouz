b02e3fcdc4e3bff6451028d7590c8bfae4087875
Karim Mahfouz <karimmahfouznd@gmail.com>
2022-09-29 13:48:49 -0400

    Test commit

---
+++ project/Coin/Coin.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+
+func _on_Coin_body_entered(_body: Node) -> void:
+	queue_free()
---
+++ project/Player/Player.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+const _GRAVITY := 200
+const _SPEED := 100
+const _JUMP_STRENGTH := 200
+
+var _velocity := Vector2.ZERO
+
+
+onready var _sprite := find_node('AnimatedSprite')
+
+func _physics_process(delta:float)->void:
+	_velocity.y += _GRAVITY * delta
+		
+	var input := Vector2.ZERO
+	input.x = Input.get_axis("Left", "Right")
+	
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		_velocity.y = -_JUMP_STRENGTH
+	
+	_velocity.x = input.x * _SPEED
+	_velocity = move_and_slide(_velocity, Vector2.UP)
+	
+	_update_animation(input)
+	
+
+func _update_animation(input:Vector2):
+	_sprite.scale.x = -1 if input.x < 0 else 1
+# warning-ignore:standalone_ternary
+	_sprite.play("idle") if input.x == 0 else _sprite.play("walk")
+	
