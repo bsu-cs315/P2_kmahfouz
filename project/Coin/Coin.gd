ceea461ef76153afaf0df846bc24bf0df479fbb5
Karim Mahfouz <karimmahfouznd@gmail.com>
2022-10-04 13:28:33 -0400

    Add coin sound effect

--- project/Coin/Coin.gd
+++ project/Coin/Coin.gd
@@ -1,4 +1,11 @@
 extends Area2D
 
+var sound_played := false
+onready var player = $AudioStreamPlayer2D
+
 func _on_Coin_body_entered(_body: Node) -> void:
+	player.play()
+	yield()
 	queue_free()
+
+
